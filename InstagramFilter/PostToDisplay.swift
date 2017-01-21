43c57e94f3637e4889d62ac3ce8c7f597342ae44
Shleb Pleb <shelbychen@shelbys-MacBook-Pro.local>
2017-01-21 14:23:55 -0800

    added location and caption vars

--- InstagramFilter/PostToDisplay.swift
+++ InstagramFilter/PostToDisplay.swift
@@ -13,18 +13,54 @@ class PostToDisplay {
     var user: String
     var likes: Int
     var followers: Int
+    var caption: String?
+    var location: String?
     
+    
+    init(image: UIImage, user: String, caption: String, location: String, likes: Int, followers: Int) {
+        self.image = image
+        self.user = user
+        self.likes = likes
+        self.followers = followers
+        self.caption = caption
+        self.location = location
+    }
+    
+    //in the case that image does not load
+    init(user: String, likes: Int, followers: Int,caption: String, location: String) {
+        self.image = nil
+        self.user = user
+        self.likes = likes
+        self.followers = followers
+        self.caption = caption
+        self.location = location
+    }
+    //in the case that caption does not load 
+    init(image: UIImage, user: String, location: String, likes: Int, followers: Int) {
+        self.caption = nil
+        self.image = image
+        self.user = user
+        self.likes = likes
+        self.followers = followers
+        self.location = location
+    }
+    //in the case that location does not load 
+    init(image: UIImage, user: String, caption: String, likes: Int, followers: Int) {
+        self.image = image
+        self.user = user
+        self.likes = likes
+        self.followers = followers
+        self.caption = caption
+        self.location = nil
+    }
+    //in the case that neither location nor caption loads
     init(image: UIImage, user: String, likes: Int, followers: Int) {
         self.image = image
         self.user = user
         self.likes = likes
         self.followers = followers
+        self.caption = nil
+        self.location = nil
     }
     
-    init(user: String, likes: Int, followers: Int) {
-        self.image = nil
-        self.user = user
-        self.likes = likes
-        self.followers = followers
-    }
 }
